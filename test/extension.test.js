7b5f55f56aff579f0a27439f7d7f988064cb67bd
Nobuhito SATO <nobuhito.sato@gmail.com>
2017-11-24 22:04:35 +0900

    add test

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,24 +1,58 @@
 /* global suite, test */
-
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 const assert = require('assert');
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 const vscode = require('vscode');
-const myExtension = require('../extension');
+const ActionLock = require('../src/actionlock');
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function() {
+suite("Extension Tests", function () {
 
-    // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("Parent checked", function () {
+        let ac = new ActionLock(true);
+        let lines = `
+- [ ] test
+- [ ] test1
+  - [x] test2
+  - [ ] test3
+    - [ ] test4
+        `.trim().split(/\r?\n/);
+
+        let tests = [
+            {
+                exp: ["[x]", "[x]"],
+                actRange: new vscode.Range(new vscode.Position(3, 4), new vscode.Position(3, 7)),
+                actDist: "[x]"
+            },
+            {
+                exp: ["[ ]", "[ ]"],
+                actRange: new vscode.Range(new vscode.Position(2, 4), new vscode.Position(2, 7)),
+                actDist: "[ ]"
+            },
+            {
+                exp: ["[x]", "[x]", "[x]"],
+                actRange: new vscode.Range(new vscode.Position(4, 6), new vscode.Position(4, 9)),
+                actDist: "[x]"
+            }
+        ];
+
+        for (const test of tests) {
+            assert.deepEqual(test.exp, ac.checkParentTasks([{ dist: test.actDist, range: test.actRange }], lines).map((d) => { return d.dist; }));
+        }
+
+        // let range1 = new vscode.Range(new vscode.Position(3, 4), new vscode.Position(3, 7));
+        // assert.deepEqual(
+        //     ["[x]", "[x]"],
+        //     ac.checkParentTasks([{ dist: "[x]", range: range1 }], lines).map(function (d) { return d.dist; })
+        // );
+
+        // let range2 = new vscode.Range(new vscode.Position(2, 4), new vscode.Position(2, 7));
+        // assert.deepEqual(
+        //     ["[ ]", "[ ]"],
+        //     ac.checkParentTasks([{ dist: "[ ]", range: range2 }], lines).map(function (d) { return d.dist; })
+        // );
+
+        // let range3 = new vscode.Range(new vscode.Position(4, 6), new vscode.Position(4, 9));
+        // assert.deepEqual(
+        //     ["[x]", "[x]", "[x]"],
+        //     ac.checkParentTasks([{ dist: "[x]", range: range3 }], lines).map(function (d) { return d.dist; })
+        // );
     });
 });
