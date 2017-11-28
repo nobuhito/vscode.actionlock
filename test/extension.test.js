619cd5acfbc6cc7349d8dcf710e44d45546d97d4
Nobuhito SATO <nobuhito.sato@gmail.com>
2017-11-28 10:25:43 +0900

    Add tests

--- test/extension.test.js
+++ test/extension.test.js
@@ -4,31 +4,105 @@ const vscode = require('vscode');
 const ActionLock = require('../src/actionlock');
 
 suite("Extension Tests", function () {
+    let lines = `
+# test
 
-    test("Parent checked", function () {
-        let ac = new ActionLock(true);
-        let lines = `
-- [ ] test
+- [ ] test 2017-11-27
 - [ ] test1
   - [x] test2
   - [ ] test3
     - [ ] test4
         `.trim().split(/\r?\n/);
 
+    let ac = new ActionLock(true);
+
+    test("Count Ranges", () => {
+        ac.makeRanges(lines);
+        assert.deepEqual(6, ac.ranges.length);
+
+        let notInstalledMDTasks = new ActionLock(false);
+        notInstalledMDTasks.makeRanges(lines);
+        assert.deepEqual(1, notInstalledMDTasks.ranges.length);
+
+    });
+
+    test("With in range", () => {
+        ac.makeRanges(lines);
+
+        let tests = [
+
+            {
+                exp: false,
+                actPosition: new vscode.Position(5, 4),
+                actDist: 4
+            },
+            {
+                exp: true,
+                actPosition: new vscode.Position(5, 5),
+                actDist: 4
+            },
+            {
+                exp: true,
+                actPosition: new vscode.Position(5, 6),
+                actDist: 4
+            },
+            {
+                exp: false,
+                actPosition: new vscode.Position(5, 7),
+                actDist: 4
+            },
+
+
+            {
+                exp: false,
+                actPosition: new vscode.Position(2, 11),
+                actDist: 0
+            },
+            {
+                exp: true,
+                actPosition: new vscode.Position(2, 12),
+                actDist: 0
+            },
+            {
+                exp: true,
+                actPosition: new vscode.Position(2, 20),
+                actDist: 0
+            },
+            {
+                exp: false,
+                actPosition: new vscode.Position(2, 21),
+                actDist: 0
+
+            }
+        ];
+
+        for (const test of tests) {
+            let range = new vscode.Range(test.actPosition, test.actPosition);
+            assert.deepEqual(test.exp, ac.isWithInRange(range, ac.ranges[test.actDist]));
+        }
+    });
+
+    test("Count QuickPickItems", () => {
+        let items = ac.buildQuickPick();
+        assert.equal(16, items.length);
+    });
+
+    test("Parent checked", () => {
+
         let tests = [
             {
                 exp: ["[x]", "[x]"],
-                actRange: new vscode.Range(new vscode.Position(3, 4), new vscode.Position(3, 7)),
+                actRange: new vscode.Range(new vscode.Position(5, 4), new vscode.Position(5, 7)),
                 actDist: "[x]"
             },
             {
                 exp: ["[ ]", "[ ]"],
-                actRange: new vscode.Range(new vscode.Position(2, 4), new vscode.Position(2, 7)),
+                actRange: new vscode.Range(new vscode.Position(4, 4), new vscode.Position(4, 7)),
                 actDist: "[ ]"
             },
             {
                 exp: ["[x]", "[x]", "[x]"],
-                actRange: new vscode.Range(new vscode.Position(4, 6), new vscode.Position(4, 9)),
+                actRange: new vscode.Range(new vscode.Position(6, 6), new vscode.Position(6, 9)),
                 actDist: "[x]"
             }
         ];
@@ -36,23 +110,5 @@ suite("Extension Tests", function () {
         for (const test of tests) {
             assert.deepEqual(test.exp, ac.checkParentTasks([{ dist: test.actDist, range: test.actRange }], lines).map((d) => { return d.dist; }));
         }
-
-        // let range1 = new vscode.Range(new vscode.Position(3, 4), new vscode.Position(3, 7));
-        // assert.deepEqual(
-        //     ["[x]", "[x]"],
-        //     ac.checkParentTasks([{ dist: "[x]", range: range1 }], lines).map(function (d) { return d.dist; })
-        // );
-
-        // let range2 = new vscode.Range(new vscode.Position(2, 4), new vscode.Position(2, 7));
-        // assert.deepEqual(
-        //     ["[ ]", "[ ]"],
-        //     ac.checkParentTasks([{ dist: "[ ]", range: range2 }], lines).map(function (d) { return d.dist; })
-        // );
-
-        // let range3 = new vscode.Range(new vscode.Position(4, 6), new vscode.Position(4, 9));
-        // assert.deepEqual(
-        //     ["[x]", "[x]", "[x]"],
-        //     ac.checkParentTasks([{ dist: "[x]", range: range3 }], lines).map(function (d) { return d.dist; })
-        // );
     });
 });
